f500f8531dedaf964900951062fd1bf0d29e7d20
aakkat <64348307+aakkat@users.noreply.github.com>
2020-10-02 14:08:16 +0530

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -2,7 +2,7 @@ job("task6_job1"){
 description("The First Job: Downloading content from GitHub")
         
 scm{
-github('devilsm13/DevOps_Task6', 'master')
+github('aakkat/DevOpsTask6', 'master')
 }
 triggers {
 scm('* * * * *')
@@ -20,9 +20,9 @@ description("The Second Job: Deploying respective webpages on the server")
 triggers {  
 upstream('task6_job1', 'SUCCESS')
 }
-steps{
-remoteShell('root@192.168.43.176:22') {
-command('''if sudo ls /root/dev3 | grep .html
+steps {
+remoteShell('root@192.168.56.107:22') {
+command('''if sudo ls /root/task3 | grep .html
 then
 if sudo kubectl get deployment | grep webserver
 then
@@ -33,16 +33,16 @@ sleep 6
 if sudo kubectl get pods | grep web
 then
 a=$(sudo kubectl get pods -o 'jsonpath={.items[0].metadata.name}')
-sudo kubectl cp /root/dev3/index.html $a:/var/www/html
+sudo kubectl cp /root/task3/index.html $a:/var/www/html
 else
 echo "Cannot copy the HTML code"
 fi
 fi
+exit;
 else
 echo "The code is not for HTML"
 fi
-
-if sudo ls /root/dev3 | grep .php
+if sudo ls /root/task3 | grep .php
 then
 if sudo kubectl get deployment | grep phpserver
 then
@@ -53,14 +53,15 @@ sleep 6
 if kubectl get pods | grep php
 then
 b=$(sudo kubectl get pods -o 'jsonpath={.items[0].metadata.name}')
-sudo kubectl cp /root/dev3/index.php $b:/var/www/html
+sudo kubectl cp /root/task3/index.php $b:/var/www/html
 else
 echo "Cannot copy the PHP code"
 fi
 fi
 else
 echo "The code is not for PHP"
-fi''')
+fi
+exit;''')
 }
 }
 }
@@ -71,11 +72,11 @@ description("The Third Job: Testing the environments")
 triggers {
 upstream('task6_job2','SUCCESS')
 }
-steps{
-remoteShell('root@192.168.43.176:22') {
-command('''if sudo kubectl get pods | grep webserver
+
+steps {
+shell ('''if sudo kubectl get pods | grep webserver
 then
-web_status_code=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.100:31000)
+web_status_code=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.102:31000)
 if [[ $web_status_code == 200 ]]
 then
 echo "The webserver is running fine"
@@ -86,10 +87,9 @@ fi
 else
 echo "No webserver running"
 fi
-
 if sudo kubectl get pods | grep phpserver
 then
-php_status_code=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.100:32000)
+php_status_code=$(curl -o /dev/null -s -w "%{http_code}" 192.168.99.102:32000)
 if [[ $php_status_code == 200 ]]
 then
 echo "The PHP server is working fine"
@@ -101,11 +101,11 @@ else
 echo "No PHP server running"
 fi''')
 }
-}
+
 
 publishers {
 extendedEmail {
-recipientList('shhhhubhammmm@gmail.com')
+recipientList('aakashkathunia@gmail.com')
 defaultSubject('Something is wrong with the build')
 defaultContent('The testing has been failed. Please Check!!')
 contentType('text/html')
